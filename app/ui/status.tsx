d849b632ee05abd0618a6002fd488116419517d3
Emmanuel Yankey <hiyankey@gmail.com>
2025-01-28 13:39:32 +0000

    Update status.tsx

--- app/ui/status.tsx
+++ app/ui/status.tsx
@@ -1,9 +1,9 @@
 export function Status() {
   return (
     <div className="relative flex h-3 w-3">
-      <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-teal-400 opacity-75" />
+      <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-orange-400 opacity-75" />
       <span
-        className="relative inline-flex h-3 w-3 rounded-full bg-teal-500"/>
+        className="relative inline-flex h-3 w-3 rounded-full bg-orange-500"/>
     </div>
   );
 }
